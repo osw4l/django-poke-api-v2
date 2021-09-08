593ce3d22da10bee6500af70a7843fa9cbe6a9ef
osw4l <ioswxd@gmail.com>
2021-09-08 10:14:20 -0500

    clean project

--- apps/utils/viewsets.py
+++ apps/utils/viewsets.py
@@ -1,9 +1,6 @@
-from rest_framework import viewsets, mixins, status
 from rest_framework.pagination import PageNumberPagination
-from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from collections import OrderedDict
-from rest_framework import filters
 
 
 class CustomPagination(PageNumberPagination):
@@ -19,28 +16,3 @@ class CustomPagination(PageNumberPagination):
             ('results', data)
         ]))
 
-
-class Osw4lPublicReadOnlyViewSet(viewsets.ReadOnlyModelViewSet):
-    permission_classes = (AllowAny,)
-    filter_backends = (filters.SearchFilter,)
-
-    def get_serializer_class(self):
-        if self.action == 'retrieve':
-            if hasattr(self, 'detail_serializer_class'):
-                return self.detail_serializer_class
-        return super().get_serializer_class()
-
-
-class Osw4lPrivateModelViewSet(mixins.CreateModelMixin,
-                              mixins.UpdateModelMixin,
-                              mixins.ListModelMixin,
-                              mixins.RetrieveModelMixin,
-                              viewsets.GenericViewSet):
-    permission_classes = (IsAuthenticated,)
-    filter_backends = (filters.SearchFilter,)
-
-    def get_serializer_class(self):
-        if self.action == 'retrieve':
-            if hasattr(self, 'detail_serializer_class'):
-                return self.detail_serializer_class
-        return super().get_serializer_class()
